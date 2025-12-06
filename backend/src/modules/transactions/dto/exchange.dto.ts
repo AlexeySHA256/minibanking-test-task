df555232219b42f1e48b146fb0effc5907a317be
AlexeyTarasov77 <alexxey2707@gmail.com>
2025-12-06 19:06:21 +0100

    Implement exchange

--- backend/src/modules/transactions/dto/exchange.dto.ts
+++ backend/src/modules/transactions/dto/exchange.dto.ts
@@ -1,2 +1,13 @@
+import { Currency } from "@/common/types";
+import { IsEnum, IsPositive } from "class-validator";
 
-export class ExchangeDto { }
+export class ExchangeDto {
+  @IsEnum(Currency)
+  from: Currency
+
+  @IsEnum(Currency)
+  to: Currency
+
+  @IsPositive()
+  amount: number
+}
--- backend/src/modules/transactions/transactions.service.ts
+++ backend/src/modules/transactions/transactions.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, ConflictException, Injectable } from '@nestjs/common';
+import { BadRequestException, ConflictException, ForbiddenException, Injectable, OnModuleInit } from '@nestjs/common';
 import { GetTransactionsRequestDto } from './dto/list.dto';
 import { TransferDto } from './dto/transfer.dto';
 import { ExchangeDto } from './dto/exchange.dto';
@@ -8,67 +8,72 @@ import { In, Repository } from 'typeorm';
 import { createPaginatedResult } from '@/common/utils/pagination.util';
 import { Account } from '@/entities/account.entity';
 import { Ledger } from '@/entities/ledger.entity';
+import { Currency } from '@/common/types';
 
 @Injectable()
-export class TransactionsService {
+export class TransactionsService implements OnModuleInit {
   @InjectRepository(Transaction) transactionRepo: Repository<Transaction>
   @InjectRepository(Account) accountRepo: Repository<Account>
 
-  async exchange(dto: ExchangeDto, userId: number) { }
+  private exchangeRates: Map<string, number>
+
+  onModuleInit() {
+    this.exchangeRates = new Map()
+    this.exchangeRates.set("USD:EUR", 0.92)
+  }
+
+  getExchangeRate(from: Currency, to: Currency) {
+    const directRate = this.exchangeRates.get(`${from}:${to}`)
+    if (directRate) return directRate
+
+    const reverseRate = this.exchangeRates.get(`${to}:${from}`)
+    if (reverseRate) return 1 / reverseRate
+  }
+
+  async exchange(dto: ExchangeDto, userId: number) {
+    const accounts = await this.accountRepo.findBy({ userId, currency: In([dto.from, dto.to]) })
+    const accountsMap = new Map(accounts.map(acc => ([acc.currency, acc])))
+    const fromAccount = accountsMap.get(dto.from)
+    const toAccount = accountsMap.get(dto.to)
+
+    if (!fromAccount) throw new BadRequestException(`You do not have a '${dto.from}' account`)
+    if (!toAccount) throw new BadRequestException(`You do not have a '${dto.to}' account`)
+
+    if (fromAccount.balance < dto.amount) throw new BadRequestException("Balance is too low")
+
+    const exchangeRate = this.getExchangeRate(dto.from, dto.to)
+    if (!exchangeRate) {
+      console.error(`No available exchange rate for ${dto.from}:${dto.to}`)
+      throw new ForbiddenException(`Exchange between ${dto.from} and ${dto.to} is not currently available`)
+    }
+    const creditAmount = dto.amount * exchangeRate
+
+    const transaction = await this.createAccountsTransaction(fromAccount.id, toAccount.id, dto.amount, TransactionType.EXCHANGE, creditAmount)
+
+    return transaction
+  }
 
   async transfer(dto: TransferDto, userId: number) {
     const fromAccount = await this.accountRepo.findOneBy({ userId, currency: dto.currency })
+
     if (!fromAccount) {
       throw new BadRequestException(`You do not have a '${dto.currency}' account`)
     }
     if (fromAccount.balance < dto.amount) throw new BadRequestException("Balance is too low")
 
+
     const toAccount = await this.accountRepo.findOneBy({ id: dto.toAccountId })
+
     if (!toAccount) {
       throw new BadRequestException("Recipient account was not found, make sure you've provided a valid account id")
     }
     if (toAccount.currency !== dto.currency) {
-      throw new BadRequestException(`Recipient account's currency does not match the selected currency. Account currency is: '${toAccount.currency}', but selected: ${dto.currency}`)
+      throw new BadRequestException(`Recipient account's currency does not match the selected currency. Account currency is: '${toAccount.currency}', but selected: '${dto.currency}'`)
     }
 
-    const result = await this.accountRepo.manager.transaction(async (dbTransaction) => {
-      await dbTransaction.insert(Ledger, { accountId: fromAccount.id, value: -dto.amount })
-      await dbTransaction.insert(Ledger, { accountId: toAccount.id, value: +dto.amount })
+    const transaction = await this.createAccountsTransaction(fromAccount.id, toAccount.id, dto.amount, TransactionType.TRANSFER)
 
-      const updateResult = await dbTransaction.query<[[], number]>(
-        'UPDATE account SET balance = balance - $1 WHERE id = $2 AND balance >= $3',
-        [dto.amount, fromAccount.id, dto.amount]
-      )
-      /* 
-       * updateResult[1] is a number of affected rows by query
-       * if from account update did not affect any rows it means that balance was updated concurrently and does not matched previously checked
-      */
-      if (updateResult[1] === 0) {
-        console.log("Transfer failed due to insufficient balance")
-        throw new ConflictException('Transaction failed, please try again')
-      }
-
-      console.log("Update result", updateResult)
-      await dbTransaction.query(
-        'UPDATE account SET balance = balance + $1 WHERE id = $2',
-        [dto.amount, toAccount.id]
-      )
-
-      return await dbTransaction.createQueryBuilder()
-        .insert()
-        .into(Transaction).values({
-          fromAccountId: fromAccount.id,
-          toAccountId: toAccount.id,
-          value: dto.amount,
-          type: TransactionType.TRANSFER
-        })
-        .returning('*')
-        .execute()
-    })
-
-    const resultRaw = result.raw as [Transaction]
-
-    return resultRaw[0]
+    return transaction
   }
 
   async getList(dto: GetTransactionsRequestDto, userId: number) {
@@ -87,5 +92,46 @@ export class TransactionsService {
     const [list, total] = await queryBuilder.getManyAndCount()
 
     return createPaginatedResult(list, total, dto.limit)
+  }
+
+  private async createAccountsTransaction(fromAccountId: string, toAccountId: string, debitAmount: number, type: TransactionType, creditAmount: number = debitAmount) {
+    const result = await this.accountRepo.manager.transaction(async (dbTransaction) => {
+      await dbTransaction.insert(Ledger, { accountId: fromAccountId, value: -debitAmount })
+      await dbTransaction.insert(Ledger, { accountId: toAccountId, value: +creditAmount })
+
+      const updateResult = await dbTransaction.query<[[], number]>(
+        'UPDATE account SET balance = balance - $1 WHERE id = $2 AND balance >= $1',
+        [debitAmount, fromAccountId]
+      )
+      /* 
+       * updateResult[1] is a number of affected rows by query
+       * if from account update did not affect any rows it means that balance was updated concurrently and does not matched previously checked
+      */
+      if (updateResult[1] === 0) {
+        console.log("Transfer failed due to insufficient balance")
+        throw new ConflictException('Transaction failed, please try again')
+      }
+
+      console.log("Update result", updateResult)
+      await dbTransaction.query(
+        'UPDATE account SET balance = balance + $1 WHERE id = $2',
+        [creditAmount, toAccountId]
+      )
+
+      return await dbTransaction.createQueryBuilder()
+        .insert()
+        .into(Transaction).values({
+          fromAccountId,
+          toAccountId,
+          value: creditAmount,
+          type
+        })
+        .returning('*')
+        .execute()
+    })
+
+    const resultRaw = result.raw as [Transaction]
+
+    return resultRaw[0]
   }
 }
